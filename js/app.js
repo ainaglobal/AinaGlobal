058de4c26e6bdb79068094410d724117cd08b2d8
AinaGlobal <ainaglobal.official@gmail.com>
2026-01-18 05:47:44 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
+// app.js
 console.log("Aina Global app.js loaded");
-// Your web app's Firebase configuration
+
 const firebaseConfig = {
   apiKey: "AIzaSyC_smT0S6OHl4jqeNnXEgnlZgSY0h0wmyw",
   authDomain: "aina-global-7cb55.firebaseapp.com",
@@ -10,6 +11,12 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-database.js";
 
-console.log("Firebase connected successfully");
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const database = getDatabase(app);
+
+console.log("Firebase Connected");
