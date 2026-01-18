8ee1fd7be7e30eaa62cad1d3be8d0245856f705e
AinaGlobal <ainaglobal.official@gmail.com>
2026-01-18 05:39:21 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,15 @@
 console.log("Aina Global app.js loaded");
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC_smT0S6OHl4jqeNnXEgnlZgSY0h0wmyw",
+  authDomain: "aina-global-7cb55.firebaseapp.com",
+  projectId: "aina-global-7cb55",
+  storageBucket: "aina-global-7cb55.firebasestorage.app",
+  messagingSenderId: "992619542791",
+  appId: "1:992619542791:web:168b4ccf4ea5adbdb4eda2"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+console.log("Firebase connected successfully");
